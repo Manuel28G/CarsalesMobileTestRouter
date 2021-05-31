5cec14da8a0190f5793e27bc64a4753fd0e2621f
Manuel Goncalves <man281192@gmail.com>
2021-05-31 11:00:21 -0400

    chore: uncomment files

--- src/main/java/com/manuel28g/carsalesmobiletestrouter/covidtestdata/RouterCovidApplication.kt
+++ src/main/java/com/manuel28g/carsalesmobiletestrouter/covidtestdata/RouterCovidApplication.kt
@@ -1,25 +1,28 @@
 package com.manuel28g.carsalesmobiletestrouter.covidtestdata
 
 import android.content.Context
+import au.com.carsales.basemodule.BaseModuleApplication
 
+import au.com.carsales.basemodule.router.BaseModuleLifeCycleManager
+import au.com.carsales.basemodule.router.Router
 
 import com.manuel28g.carsalesmobiletestdata.service.CovidDataService
 
-class RouterCovidApplication{
+class RouterCovidApplication:BaseModuleApplication(){
 
-//    override fun initLifeCycleManager(): BaseModuleLifeCycleManager {
-//       return BaseModuleLifeCycleManager(this)
-//    }
-//
-//    override fun onCreate() {
-//        super.onCreate()
-//    }
+    override fun initLifeCycleManager(): BaseModuleLifeCycleManager {
+       return BaseModuleLifeCycleManager(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
 }
 
 var mobileTestData: CovidDataService? = null
 
-//fun Context.getMobileTestData(): CovidDataService? {
-//    if (mobileTestData == null)
-//        mobileTestData = Router.getService(CovidDataService::class.java)
-//    return mobileTestData
-//}
+fun Context.getMobileTestData(): CovidDataService? {
+    if (mobileTestData == null)
+        mobileTestData = Router.getService(CovidDataService::class.java)
+    return mobileTestData
+}
