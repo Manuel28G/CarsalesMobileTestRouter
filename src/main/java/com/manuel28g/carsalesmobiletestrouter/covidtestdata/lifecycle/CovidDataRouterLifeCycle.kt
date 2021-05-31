f2efd365cccbe95969dc264a204d681c222c2268
Manuel Goncalves <man281192@gmail.com>
2021-05-31 11:00:21 -0400

    chore: uncomment files

--- src/main/java/com/manuel28g/carsalesmobiletestrouter/covidtestdata/lifecycle/CovidDataRouterLifeCycle.kt
+++ src/main/java/com/manuel28g/carsalesmobiletestrouter/covidtestdata/lifecycle/CovidDataRouterLifeCycle.kt
@@ -1,14 +1,18 @@
 package com.manuel28g.carsalesmobiletestrouter.covidtestdata.lifecycle
 
 import android.app.Application
+
+import au.com.carsales.basemodule.router.BaseModuleLifeCycle
+import au.com.carsales.basemodule.router.IModuleConfig
+
 import com.manuel28g.carsalesmobiletestdata.service.CovidDataService
 import com.manuel28g.carsalesmobiletestdata.service.CovidDataServiceImpl
 
-class CovidDataRouterLifeCycle(application: Application){
-//    BaseModuleLifeCycle(application)  {
-//
-//    override fun onCreate(config: IModuleConfig) {
-//        super.onCreate(config)
-//        config.registerService(CovidDataService::class.java, CovidDataServiceImpl::class.java)
-//    }
+open class CovidDataRouterLifeCycle(application: Application):
+    BaseModuleLifeCycle(application)  {
+
+    override fun onCreate(config: IModuleConfig) {
+        super.onCreate(config)
+        config.registerService(CovidDataService::class.java, CovidDataServiceImpl::class.java)
+    }
 }
